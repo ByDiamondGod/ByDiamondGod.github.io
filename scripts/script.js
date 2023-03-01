eac9996d77a8ce94b2e6b9ac3808b2330727fe45
Eddev <eduardemian777@gmail.com>
2023-02-28 23:11:57 -0300

    responsive  maintenance and changes (header - introduction - services)

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,20 @@ const nav = document.querySelector(".header__nav");
 button.addEventListener("click", () => {
   nav.classList.toggle("header__nav--active");
 });
+
+
+
+/* const open = document.querySelector(".mob-open");
+const close = document.querySelector(".mob-close");
+
+const wrapper = document.querySelector(".mob-wrapper").classList;
+
+open.addEventListener("click", () => {
+  wrapper.toggle("opened");
+  wrapper.toggle("close");
+});
+
+close.addEventListener("click", () => {
+  wrapper.toggle("opened");
+  wrapper.toggle("close");
+}); */
