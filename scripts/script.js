e02fa199cdf01c19a69e91efecf4279ceae81ac0
Eddev <eduardemian777@gmail.com>
2023-03-10 15:48:24 -0300

    add scroll interactive AOS

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,12 @@
-
-/* Adding a class to the nav element when the button is clicked. */
+/* Adding a class to the nav element when the button is clicked.----------- */
 const button = document.querySelector(".menu-btn");
 const nav = document.querySelector(".header__nav");
 
 button.addEventListener("click", () => {
-  nav.classList.toggle("header__nav--active");
+  nav.classList.toggle("header__nav--desactive");
 });
 
-
-/* This is the code that is making the hamburger menu button animate. */
+/* This is the code that is making the hamburger menu button animate.------- */
 const menuBtn = document.querySelector(".menu-btn");
 let menuOpen = false;
 
